394b2ccb717a5ead3ee02443bc9e295ace89e293
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-14 09:08:07 +0300

    Get scrapes done

--- controllers/scrape.js
+++ controllers/scrape.js
@@ -1,7 +1,23 @@
+const Scrape = require("../models/Scrape");
 const catchAsync = require("../utils/catchAsync");
 
 module.exports = {
     getScrapes: catchAsync(async (req, res) => {
+        if (!req.body.investorId)
+            return res.status(400).send({
+                success: false,
+                message: "Please investor id",
+            });
+        const { investorId } = req.body;
+        const scrapes = await Scrape.find({ investorId });
+        // select expiclity password
+
+        res.status(200).json({
+            success: true,
+            message: `Successfull.`,
+            scrapes,
+        });
+
         res.send({ scrapes: [] });
     }),
 
