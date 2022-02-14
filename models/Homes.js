408196ba770c4d08b341d57ae0bd0b05d9c11e09
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-14 10:54:59 +0300

    Home Schema

---
+++ models/Homes.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const homesSchema = new mongoose.Schema({
+    investorId: {
+        type: String,
+        required: true,
+    },
+    scraped: {
+        type: Boolean,
+        required: true,
+    },
+    category: {
+        type: Boolean,
+        enum: ["prospect", "active", "managed", "archived", "scrapped"],
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    bedrooms: {
+        type: String,
+        required: true,
+    },
+    bathrooms: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    images: {
+        type: [String],
+        required: true,
+    },
+    createdAt: {
+        type: String,
+        default: Date.now(),
+    },
+});
+
+const Home = mongoose.model("Home", homesSchema);
+module.exports = Home;
