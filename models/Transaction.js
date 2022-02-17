c6f3c461f982bcd2c08866315f3ef3b4bacc742e
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-17 12:11:26 +0300

    Transaction models

---
+++ models/Transaction.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    homeId: {
+        type: String,
+        required: true,
+    },
+    investorId: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ["income", "expense"],
+        required: true,
+    },
+    recurring: {
+        type: Boolean,
+        required: true,
+    },
+    startDate: {
+        type: String,
+        default: "",
+    },
+    endDate: {
+        type: String,
+        default: "",
+    },
+    createdAt: {
+        type: String,
+        default: Date.now(),
+    },
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+module.exports = Transaction;
